6a0b47035c2a32a0d4ca8321d0978ab5881b5baf
Mohammad Movahedmanesh <m.movahedmanesh@mail.sbu.ac.ir>
2019-04-24 13:53:37 +0430

    initial commit v.2

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,18 @@
 <template>
   <div>
     <div>your name is: {{ name }}</div>
-    <input v-model="name" />
-    <div class="" id="" />
+    <input v-model="name" aria-label="299" name="name" />
     <v-avatar size="40">
-      <img src="https://picsum.photos/70" />
+      <img src="https://picsum.photos/70" alt="img" />
     </v-avatar>
   </div>
 </template>
-<script lang="js">
-import { VAvatar } from "vuetify/lib"
-// import { Vue, Component } from 'vue-property-decorator'
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
 
-export default {
-  component: {
-    VAvatar
-  },
-  data: () => ({
-    name: 'mamad'
-  })
+@Component
+export default class Index extends Vue {
+  name: string = 'sjiojoiwjiofj'
 }
 </script>
 <style></style>
--- pages/salam.vue
+++ pages/salam.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name: 'salam.vue'
+  name: 'Salam'
 }
 </script>
 
