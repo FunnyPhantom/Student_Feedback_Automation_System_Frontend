51aa6725c2196eb081b029cb1f97d6e08e878f1a
Mohammad Movahedmanesh <m.movahedmanesh@mail.sbu.ac.ir>
2019-04-24 13:20:34 +0430

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>your name is: {{ name }}</div>
+    <input v-model="name" />
+    <div class="" id="" />
+    <v-avatar size="40">
+      <img src="https://picsum.photos/70" />
+    </v-avatar>
+  </div>
+</template>
+<script lang="js">
+import { VAvatar } from "vuetify/lib"
+// import { Vue, Component } from 'vue-property-decorator'
+
+export default {
+  component: {
+    VAvatar
+  },
+  data: () => ({
+    name: 'mamad'
+  })
+}
+</script>
+<style></style>
---
+++ pages/salam.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="display-4">
+      hello to this page.
+    </div>
+    <div>this page route is: {{ $route.path }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'salam.vue'
+}
+</script>
+
+<style scoped></style>
